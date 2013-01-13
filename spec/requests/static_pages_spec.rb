f40e40576cbc369c83769d7041a2bf07a598386a
M. Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-01-13 15:14:22 +0100

    new spec directory

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+#  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
-  subject {page }
+  subject { page }
 
   describe "Home Page" do
     before { visit root_path }
 
     it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: "#{base_title}") }
+    it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
   end
 
@@ -18,20 +18,20 @@ describe "StaticPages" do
     before { visit help_path }
 
     it { should have_content('Help') }
-    it { should have_selector('title', text: "#{base_title} | Help") }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
   
     it { should have_content('About Us') }
-    it { should have_selector('title', text: "#{base_title} | About Us") }
+    it { should have_selector('title', text: full_title('About')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
   
     it { should have_content('Contact') }
-    it { should have_selector('title', text: "#{base_title} | Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
