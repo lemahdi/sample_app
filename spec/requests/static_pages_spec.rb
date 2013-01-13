0bbee5e9e1832b1d3015e53a8c6d17dec5903c99
M. Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-01-13 15:07:14 +0100

    refactoring specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,53 +7,29 @@ describe "StaticPages" do
   describe "Home Page" do
     before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      page.should have_selector('h1', text: 'Sample App')
-    end
-
-    it "should have the base title" do
-      page.should have_selector('title', :text => "#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      page.should_not have_selector('title', :text => '| Home')
-    end
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: "#{base_title}") }
+    it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      page.should have_content('Help')
-    end
-
-    it "should have the right title 'Help'" do
-      page.should have_selector('title', :text => "#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_selector('title', text: "#{base_title} | Help") }
   end
 
   describe "About page" do
     before { visit about_path }
   
-    it "should have the content 'About Us'" do
-      page.should have_content('About Us')
-    end
-
-    it "should have the right title 'About Us'" do
-      page.should have_selector('title', :text => "#{base_title} | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_selector('title', text: "#{base_title} | About Us") }
   end
 
   describe "Contact page" do
     before { visit contact_path }
   
-    it "should have the content 'Contact'" do
-      page.should have_content('Contact')
-    end
-
-    it "should have the right title 'Contact'" do
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_selector('title', text: "#{base_title} | Contact") }
   end
 end
