5566a63f63f04ea11eaf5b6f39570ef318d48cab
M. Mahdi Akkouh <akkouh.mahdi@gmail.com>
2013-01-20 15:04:37 +0100

    correcting some links and methods for signin functionality

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,8 +7,8 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { shoudl have_selector('h1',    text: 'Sign in') }
-    it { shoudl have_selector('title', text: 'Sign in') }
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
   end
 
   describe "signin" do
